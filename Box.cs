5592e22ebf57fc984ab346748596f6d7e30b5946
Dj <ritchey0713@gmail.com>
2020-02-20 13:13:26 -0500

    add display method

--- Box.cs
+++ Box.cs
@@ -1,6 +1,18 @@
-namespace oopLearn
-{
-    public class Box {
-        
-    }
+using System;
+
+namespace oopLearn {
+	public class Box {
+		// member variables
+		public int length;
+		public int height;
+		public int width;
+		public int volume;
+
+		public void DisplayInfo() {
+			System.Console.WriteLine("Length is {0}, Height is {1}, Width is {2} and volume is {3}",
+			 length, height, width, volume = length * height * width);
+		}
+
+
+	}
 }
