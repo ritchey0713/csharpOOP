31f4e8031ac64ef731b7373772d47125e58cf735
Dj <ritchey0713@gmail.com>
2020-02-20 12:28:32 -0500

    add constructor

--- Human.cs
+++ Human.cs
@@ -2,13 +2,26 @@ using System;
 
 namespace oopLearn
 {
-    public class Human {
-			//member variable
-			public string name;
+	public class Human {
+		//member variable
+		private string firstName;
+		private string lastName;
 
-			//member method
-			public void IntroduceMyself(){
-				System.Console.WriteLine("Hi my name is {0}", name);
-			}
-    }
+		//constructor
+		public Human(string firstName, string lastName) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+		}
+
+		//default assignment
+		//public string name = "Jon";
+
+		// when private cant be changed from other places
+		//private string name = "Anon";
+
+		//member method
+		public void IntroduceMyself(){
+			System.Console.WriteLine("Hi my name is {0} {1}", firstName, lastName);
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -2,13 +2,21 @@
 
 namespace oopLearn
 {
-    class Program
-    {
-        static void Main(string[] args) {
-          // create a new instance
-          Human jon = new Human();
-          jon.name = "Jon";
-          jon.IntroduceMyself();
-        }
-    }
+  class Program {
+		static void Main(string[] args) {
+			// create a new instance
+			Human jon = new Human("John", "Smith");
+			//assign member variable
+			// jon.firstName = "Jon";
+			// jon.lastName = "Smith";
+			//call method duh
+			jon.IntroduceMyself();
+
+			Human michael = new Human("Michael", "Richards");
+			// michael.firstName = "Michael";
+			// michael.lastName = "Richards";
+
+			michael.IntroduceMyself();
+		}
+  }
 }
