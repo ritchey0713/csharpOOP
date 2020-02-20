b799b54f6dddc7885560661eb0058f03f27e2639
Dj <ritchey0713@gmail.com>
2020-02-20 11:55:10 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oopLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
