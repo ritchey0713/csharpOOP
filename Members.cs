56c3092071b773619fd53ec0a0797255a863d08a
Dj <ritchey0713@gmail.com>
2020-02-21 17:11:54 -0500

    add finalizer

--- Members.cs
+++ Members.cs
@@ -1,12 +1,16 @@
+using System.Diagnostics;
+
 namespace oopLearn {
     public class Members {
 			// private member / field 
 			private string memberName;
 			private string jobtitle;
+			private int salary = 60000;
 
 			//public member/ field 
 			public int age;
 
+		//exposes job title private member
 			public string JobTitle {
 				get{
 					return this.jobtitle;
@@ -15,5 +19,34 @@ namespace oopLearn {
 					this.jobtitle = value;
 				}
 			}
+			// public member method can be called from any class
+			public void Introducing(bool isFriend) {
+				if(isFriend) {
+					SharingPrivateInfo();
+				} else {
+					System.Console.WriteLine("Hi my name is {0}, I am a {1}", this.memberName, this.jobtitle);
+				}
+			}
+
+			private void SharingPrivateInfo(){
+				System.Console.WriteLine("My Salary is {0}", this.salary);
+			}
+
+			// constructor 
+			public Members() {
+				age = 30;
+				memberName = "Sally";
+				salary = 75000;
+				jobtitle = "Developer";
+				System.Console.WriteLine("Member created!");
+			}
+
+			//finalizer/ destructor can only have one
+			~Members(){
+				// clean up statements 
+				System.Console.WriteLine("Deconstruction of member complete");
+				// Debug.Write("Deconstruction of member complete");
+			}
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -32,12 +32,16 @@ namespace oopLearn
 
 			// tim.IntroduceMyself();
 
-			Box box1 = new Box(10, 10, 10);
+			// Box box1 = new Box(10, 10, 10);
 
-			box1.Width = 20;
-			System.Console.WriteLine("Box volume is {0}", box1.Volume);
-			box1.DisplayInfo();
-			System.Console.WriteLine(box1.FrontSurface);
+			// box1.Width = 20;
+			// System.Console.WriteLine("Box volume is {0}", box1.Volume);
+			// box1.DisplayInfo();
+			// System.Console.WriteLine(box1.FrontSurface);
+
+			// Members member1 = new Members();
+			// member1.Introducing(true);
+			
 		}
   }
 }
