d8581d38dd2409102e4713c313947c9a60bd4075
Dj <ritchey0713@gmail.com>
2020-02-21 14:49:46 -0500

    build members out

--- Members.cs
+++ Members.cs
@@ -1,5 +1,19 @@
 namespace oopLearn {
     public class Members {
-        
+			// private member / field 
+			private string memberName;
+			private string jobtitle;
+
+			//public member/ field 
+			public int age;
+
+			public string JobTitle {
+				get{
+					return this.jobtitle;
+				}
+				set{
+					this.jobtitle = value;
+				}
+			}
     }
 }
