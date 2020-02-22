fcbc63c931af317e1e62a62d7391d05af903b674
Dj <ritchey0713@gmail.com>
2020-02-21 22:08:35 -0500

    add notes

--- Program.cs
+++ Program.cs
@@ -43,125 +43,153 @@ namespace oopLearn
 			// member1.Introducing(true);
 
 			// creating arrays 
-			int[] grades = new int[5];
-			grades[0] = 20;
-			grades[1] = 15;
-			grades[2] = 10;
-			grades[3] = 20;
-			grades[4] = 10;
-
-			System.Console.WriteLine(grades[3]);
-			
-			
-			// string input = Console.ReadLine();
-
-			// grades[3] = int.Parse(input);
+			// int[] grades = new int[5];
+			// grades[0] = 20;
+			// grades[1] = 15;
+			// grades[2] = 10;
+			// grades[3] = 20;
+			// grades[4] = 10;
 
 			// System.Console.WriteLine(grades[3]);
+			
+			
+			// // string input = Console.ReadLine();
 
-			// int[] mathStudentsGrades = {20, 13, 21, 50, 9, 10};
+			// // grades[3] = int.Parse(input);
 
-			// int[] mathStudentsGradesB = new int[] {20, 10, 6, 7, 30 };
+			// // System.Console.WriteLine(grades[3]);
 
-			// System.Console.WriteLine("Length of studen grades {0}", mathStudentsGrades.Length);
+			// // int[] mathStudentsGrades = {20, 13, 21, 50, 9, 10};
 
-			int[] nums = new int[10];
+			// // int[] mathStudentsGradesB = new int[] {20, 10, 6, 7, 30 };
 
-			for(int i = 0; i < nums.Length; i++){
-				nums[i] = i;
-			}
+			// // System.Console.WriteLine("Length of studen grades {0}", mathStudentsGrades.Length);
 
-			for(int j = 0; j < nums.Length; j++){
-				System.Console.WriteLine(nums[j]);
-			}
+			// int[] nums = new int[10];
 
-			//use counter to get index
-			int counter = 0;
-			foreach(int k in nums){
-				counter = k;
-				System.Console.WriteLine("element {0} =  {1}", counter, k);
-			}
+			// for(int i = 0; i < nums.Length; i++){
+			// 	nums[i] = i;
+			// }
 
-			string[] friends = new string[5];
-			friends[0] = "Brax";
-			friends[1] = "Chris";
-			friends[2] = "Erika";
-			friends[3] = "Tyler";
-			friends[4] = "Mike";
+			// for(int j = 0; j < nums.Length; j++){
+			// 	System.Console.WriteLine(nums[j]);
+			// }
 
-			foreach(string x in friends){
-				System.Console.WriteLine("Hey there, {0}!", x);
-			}
+			// //use counter to get index
+			// int counter = 0;
+			// foreach(int k in nums){
+			// 	counter = k;
+			// 	System.Console.WriteLine("element {0} =  {1}", counter, k);
+			// }
 
-			// declare 2d array 
-			string[,] matrix;
+			// string[] friends = new string[5];
+			// friends[0] = "Brax";
+			// friends[1] = "Chris";
+			// friends[2] = "Erika";
+			// friends[3] = "Tyler";
+			// friends[4] = "Mike";
 
-			//3d array 
-			int[,,] threeD;
+			// foreach(string x in friends){
+			// 	System.Console.WriteLine("Hey there, {0}!", x);
+			// }
 
-			//2d array 
-			int[,] array2D = new int[,] {
-				{
-					1, 2, 3 // row 0
-				},
-				{
-					4, 5, 6 //row 1
-				},
-				{
-					7, 8, 9  //row 2
-				}
-			};
+			// // declare 2d array 
+			// string[,] matrix;
 
-			System.Console.WriteLine("central value is {0}", array2D[1,1]);
+			// // doesnt work
+			// // matrix = {
+			// // 	{"1", "2"},
+			// // 	{"3", "4"}
+			// // }
 
-			//3d array must have same amount of rows in both
-			string[,,] array3D = new string[,,] {
-				{
-					{
-						"1", "2", "3"
-					},
-					{
-						"4", "5", "6"
-					},
-					{
-						"Hello!", "Hi", "what's up!"
-					}
-				},
-				{
-					{
-						"10", "11", "12"
-					},
-					{
-						"13", "14", "15"
-					},
-					{
-						"Goodbye!", "see ya!", "Later!"
-					}
-				}
-			};
+			// //3d array 
+			// int[,,] threeD;
 
-			System.Console.WriteLine("to get the 5 is {0}", array3D[0,1,1]);
+			// //2d array 
+			// int[,] array2D = new int[,] {
+			// 	{
+			// 		1, 2, 3 // row 0
+			// 	},
+			// 	{
+			// 		4, 5, 6 //row 1
+			// 	},
+			// 	{
+			// 		7, 8, 9  //row 2
+			// 	}
+			// };
 
-			System.Console.WriteLine("to get the 11 is {0}", array3D[1,0,1]);
+			// System.Console.WriteLine("central value is {0}", array2D[1,1]);
 
-			System.Console.WriteLine("Say {0}!", array3D[0,2,0]);
+			// //3d array must have same amount of rows in both
+			// string[,,] array3D = new string[,,] {
+			// 	{
+			// 		{
+			// 			"1", "2", "3"
+			// 		},
+			// 		{
+			// 			"4", "5", "6"
+			// 		},
+			// 		{
+			// 			"Hello!", "Hi", "what's up!"
+			// 		}
+			// 	},
+			// 	{
+			// 		{
+			// 			"10", "11", "12"
+			// 		},
+			// 		{
+			// 			"13", "14", "15"
+			// 		},
+			// 		{
+			// 			"Goodbye!", "see ya!", "Later!"
+			// 		}
+			// 	}
+			// };
 
-			System.Console.WriteLine("and now say {0}!", array3D[1,2,0]);
+			// System.Console.WriteLine("to get the 5 is {0}", array3D[0,1,1]);
+
+			// System.Console.WriteLine("to get the 11 is {0}", array3D[1,0,1]);
+
+			// System.Console.WriteLine("Say {0}!", array3D[0,2,0]);
+
+			// System.Console.WriteLine("and now say {0}!", array3D[1,2,0]);
 
 
-			//specify demensions of matrix 
+			// //specify demensions of matrix 
 
-			string[,] array2D2 = new string[3,2]{
-				{
-					"1", "2"
-				},
-				{
-					"3", "4"
-				},
-				{
-					"5", "6"
-				}
-			};
+			// string[,] array2D2 = new string[3,2]{
+			// 	{
+			// 		"1", "2"
+			// 	},
+			// 	{
+			// 		"3", "4"
+			// 	},
+			// 	{
+			// 		"5", "6"
+			// 	}
+			// };
+
+			// array2D2[1,1] = "Chicken!";
+			// System.Console.WriteLine(array2D2[1,1]);
+
+			// //find matrix dimensions 
+			// int demensions = array2D2.Rank;
+
+			// System.Console.WriteLine("Dementions are {0}", demensions);
+
+			// string[,] array2D3 = {
+			// 	{
+			// 		"1", "2"
+			// 	},
+			// 	{
+			// 		"3", "4"
+			// 	},
+			// 	{
+			// 		"5", "6"
+			// 	}
+			// };
+
+			
 
 		}
   }
