ac0f026835b3ce173876120c0137adad2271fb39
Dj <ritchey0713@gmail.com>
2020-03-03 19:34:38 -0500

    add new classes

---
+++ University.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oopLearn
+{
+  class University
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public void Print()
+    {
+      Console.WriteLine("University {0} with id of {1}", Name, Id);
+    }
+  }
+}
