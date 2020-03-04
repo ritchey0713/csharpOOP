d64a67d57899e8d30ed31903a69391a0dfc3062b
Dj <ritchey0713@gmail.com>
2020-03-03 19:37:45 -0500

    add student attrs

--- Student.cs
+++ Student.cs
@@ -6,5 +6,18 @@ namespace oopLearn
 {
   class Student
   {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public int Age { get; set; }
+
+    // foreign key 
+    public int UniversityId { get; set; }
+
+    public void Print()
+    {
+      Console.WriteLine("Student {0}, with id {1}, gender {2} age {3} university id {4}", 
+        Name, Id, Gender, Age, UniversityId);
+    }
   }
 }
